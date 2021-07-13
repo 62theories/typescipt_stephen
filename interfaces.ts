1b74be42b9b68290b13b0b80bf8f9cb3ce8077c6
ณัฐวัฒน์ สงสม <nattawatx86@gmail.com>
2021-07-13 16:32:37 +0700

    map

---
+++ interfaces.ts
@@ -0,0 +1,24 @@
+const oldCivic = {
+  model: "civic",
+  year: 2000,
+  broken: true,
+}
+
+// 1 simple
+// const printVehicle = (vehicle: {
+//   model: string
+//   year: number
+//   broken: boolean
+// }): void => {
+//   console.log(vehicle.model)
+// }
+
+// 2 interface
+// interface Vehicle {
+//   model: string
+//   year: number
+//   broken: boolean
+// }
+// const printVehicle = (vehicle: Vehicle): void => {
+//   console.log(vehicle.model)
+// }
